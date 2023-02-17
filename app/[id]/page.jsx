d43de7176cfa87614c12cf51835a054a1b4c542e
umman <ummanw@yahoo.com>
2023-02-17 20:14:08 +0500

    resolved caching issue

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-async function ProductPage({ params }) {
+async function getProduct(id) {
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/${params.id}`,
+    `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/${id}`,
     {
+      cache: "no-store",
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -12,8 +13,13 @@ async function ProductPage({ params }) {
     }
   );
 
-  const product = await res.json();
-  // const product = await getProduct(params.id);
+  const data = await res.json();
+
+  return data;
+}
+
+async function ProductPage({ params }) {
+  const product = await getProduct(params.id);
 
   return (
     <div>
