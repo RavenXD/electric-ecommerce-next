9ab9e0484ad73a2ea6d087ca9e8cca686df022bf
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-02-27 20:33:02 +0500

    Add react-multi-carousel to the categories slider

--- app/(Components)/Categories.jsx
+++ app/(Components)/Categories.jsx
@@ -1,5 +1,27 @@
 "use client";
 import React, { useState } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  // superLargeDesktop: {
+  //   // the naming can be any, depends on you.
+  //   breakpoint: { max: 4000, min: 3000 },
+  //   items: 5
+  // },
+  // desktop: {
+  //   breakpoint: { max: 3000, min: 1024 },
+  //   items: 3
+  // },
+  // tablet: {
+  //   breakpoint: { max: 1024, min: 464 },
+  //   items: 2
+  // },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 4
+  }
+};
 
 function Categories() {
   const [isActive, setActive] = useState("All");
@@ -9,7 +31,7 @@ function Categories() {
       <div>
         <h3 className="m-4 text-2xl">Categories</h3>
       </div>
-      <div className=" flex mx-4 justify-between ">
+      <Carousel responsive={responsive} removeArrowOnDeviceType={["tablet", "mobile"]} className=" flex mx-4 justify-between">
         <button
           className={` text-xs py-2 px-5 rounded-xl border text-gray-500 ${
             isActive == "All" ? " bg-black text-white" : ""
@@ -41,8 +63,23 @@ function Categories() {
           onClick={() => setActive("Frill")}
         >
           Frill
+        </button><button
+          className={` text-xs py-2 px-5 rounded-xl border text-gray-500 ${
+            isActive == "Scissors" ? " bg-black text-white" : ""
+          }`}
+          onClick={() => setActive("Scissors")}
+        >
+          Scissors
         </button>
-      </div>
+        <button
+          className={` text-xs py-2 px-5 rounded-xl border text-gray-500 ${
+            isActive == "Pearls" ? " bg-black text-white" : ""
+          }`}
+          onClick={() => setActive("Pearls")}
+        >
+          Pearls
+        </button>
+      </Carousel>
     </>
   );
 }
