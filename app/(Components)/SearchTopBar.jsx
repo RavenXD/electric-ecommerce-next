34b4f2ca00b7ba65ab8d9ea4c1110e3e427c16c7
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-03 19:25:55 +0500

    add Back arrow

--- app/(Components)/SearchTopBar.jsx
+++ app/(Components)/SearchTopBar.jsx
@@ -1,5 +1,6 @@
 "use client";
-
+import Image from "next/image";
+import backArrow from "../../assets/back-arrow.png"
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
@@ -7,7 +8,9 @@ export default function SearchTopBar() {
   const router = useRouter();
   return (
     <div className=" p-4 mt-20 flex justify-between items-center bg-gray-100">
-      <button className="font-bold text-sm" onClick={router.back}>{`<Back`}</button>
+      <button className="font-bold text-sm" onClick={router.back}>
+        <Image className="rounded-lg drop-shadow-md " width={30} height={30} src={backArrow} alt="back arrow" />
+      </button>
       <h3 className="font-bold ">Search</h3>
       <Link className="font-bold text-sm" href="/">
         Home
