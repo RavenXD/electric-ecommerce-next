a8879f9bb3659f1b395a6f4877ba81a5d0235cce
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-04 20:57:30 +0500

    Update BackButton.jsx

--- app/(Components)/BackButton.jsx
+++ app/(Components)/BackButton.jsx
@@ -1,23 +1,13 @@
 "use client";
-
+import Image from "next/image";
+import backArrow from "../../assets/back-arrow.png"
 import { useRouter } from "next/navigation";
 
 function BackButton() {
   const router = useRouter();
   return (
     <button onClick={router.back} className="absolute top-3 left-3 ">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-8 h-10"
-      >
-        <path
-          fillRule="evenodd"
-          d="M18 10a.75.75 0 01-.75.75H4.66l2.1 1.95a.75.75 0 11-1.02 1.1l-3.5-3.25a.75.75 0 010-1.1l3.5-3.25a.75.75 0 111.02 1.1l-2.1 1.95h12.59A.75.75 0 0118 10z"
-          clipRule="evenodd"
-        />
-      </svg>
+      <Image className="rounded-lg drop-shadow-md " width={30} height={30} src={backArrow} alt="back arrow" />
     </button>
   );
 }
